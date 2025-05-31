240ceafce33c838f297e34049391ccac86069738
nallasathvika <nallasathvika05@gmail.com>
2025-05-31 18:20:24 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3010;
 
+job.start()
 // Middleware
 app.use(express.json());
 app.use("/api/auth", authRoutes);
