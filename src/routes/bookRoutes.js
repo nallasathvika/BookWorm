eb7086061cff5d4b8c4553771b1e211cad54e242
nallasathvika <nallasathvika05@gmail.com>
2025-07-03 11:10:05 +0530

    change

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -33,7 +33,8 @@ router.post("/",protectRoute, async(req,res)=>{
 router.get("/",protectRoute,async(req,res)=>{
     //const response=await fetch("http://localhost:3010/api/books?page=1&limit=5");
     try{
-        const page=req.query.limit||5;
+        const page =req.query.page||1;
+        const limit=req.query.limit||5;
         const skip=(page-1)*limit;
         const books=await Book.find().sort({createdAt:-1}).skip(skip).limit(limit).populate("user","username profileImage");
         const totalBooks=await Book.countDocuments();
