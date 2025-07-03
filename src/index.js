56c304968387b9ed3cead490b30cfdeda1124624
nallasathvika <nallasathvika05@gmail.com>
2025-07-03 10:54:27 +0530

    change

--- src/index.js
+++ src/index.js
@@ -6,18 +6,16 @@ import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 3010;
-
-job.start()
+job.start();
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/books",bookRoutes);
 
 // Connect DB and then start server
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
